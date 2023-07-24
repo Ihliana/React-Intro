daf633fcee4e4c6b4b5f98410e6dd951b93fbdf3
Ihliana <Ihnatco@protonmail.com>
2023-07-24 13:34:33 +0200

    Adding Fun Facts
    Adding a list with Fun facts about React and testing the application

--- src/App.test.js
+++ src/App.test.js
@@ -6,6 +6,16 @@ import App from './App';
 
 describe("App renders corretly", () =>{
 
+  test('renders React logo with correct attributes', () => {
+    render(<App />)
+
+    const logoElement = screen.getByAltText("React Logo")
+
+    expect(logoElement).toBeInTheDocument()
+    expect(logoElement).toHaveAttribute("src", 'react-logo.png')
+    expect(logoElement).toHaveAttribute('width', '40px')
+  })
+
   test('renders page heading', () => {
     render(<App />);
     
@@ -13,15 +23,27 @@ describe("App renders corretly", () =>{
       level: 1
     });
     expect(pageHeading).toBeInTheDocument();
-
-
-    const headingContent = screen.getByText("Fun facts about React")
-    expect(headingContent).toBeInTheDocument();
+    expect(pageHeading).toHaveTextContent("Fun facts about React");
 
   });
 
+  test('renders all the fun facts', () => {
+    render(<App />)
 
+    const funFacts = screen.getAllByRole("listitem")
 
+    const expectedFacts = [
+      'Was first released in 2013',
+      'Was originally created by Jordan Walke',
+      'Has well over 100K stars on Github',
+      'Is maintained by Facebook',
+      'Powers thousands of enterprise apps, including mobile apps',
+    ];
+
+    funFacts.forEach((fact, index) => {
+      expect(fact).toHaveTextContent(expectedFacts[index])
+    })
+  })
 
 
 })
