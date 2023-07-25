a2ace98ca203d936917af407304312cda525370d
Ihliana <Ihnatco@protonmail.com>
2023-07-25 08:52:35 +0200

    Testing 1
    Testing Header component

---
+++ src/components/Header.test.js
@@ -0,0 +1,40 @@
+import {render, screen} from "@testing-library/react"
+
+import Header from "./Header"
+
+
+describe("Header component testing suite", () => {
+    test('renders header elements', () => {
+        render(<Header />)
+
+        //specific elements 
+
+        const h3Heading = screen.getByRole('heading', {
+            level: 3
+        })
+        const h4Heading = screen.getByRole("heading", {
+            level: 4
+        })
+        const reactLogo = screen.getByAltText("React Logo")
+        const reactLogoText = screen.getByText(/React Facts/i)
+        const reactCourseTitle = screen.getByText(/React Course - Project 1/i)
+    
+
+        //assertions 
+        expect(h3Heading).toBeInTheDocument()
+        expect(h4Heading).toBeInTheDocument()
+        expect(reactLogo).toBeInTheDocument();
+        expect(reactLogoText).toBeInTheDocument();
+        expect(reactCourseTitle).toBeInTheDocument();
+
+    })
+
+    test("header's CSS classes", () => {
+        const {container} = render(<Header />)
+
+        expect(container.querySelector('.nav')).toHaveClass('nav')
+    })
+
+
+
+})
